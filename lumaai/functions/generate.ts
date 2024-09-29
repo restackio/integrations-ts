3692c0ae9ec5106e58b783c2ed78088212f267c3
aboutphilippe <philippemarketing@gmail.com>
2024-09-29 12:06:28 +0200

    extend

--- lumaai/functions/generate.ts
+++ lumaai/functions/generate.ts
@@ -6,10 +6,12 @@ export async function lumaaiGenerate({
   prompt,
   aspectRatio,
   apiKey,
+  extendGenerationId,
 }: {
   prompt: string;
   aspectRatio: "1:1" | "16:9" | "9:16" | "4:3" | "3:4" | "21:9" | "9:21";
   apiKey?: string;
+  extendGenerationId?: string;
 }) {
   try {
     const client = lumaaiClient({
@@ -20,6 +22,16 @@ export async function lumaaiGenerate({
       .create({
         aspect_ratio: aspectRatio,
         prompt: prompt,
+        ...(extendGenerationId
+          ? {
+              keyframes: {
+                frame0: {
+                  type: "generation",
+                  id: extendGenerationId,
+                },
+              },
+            }
+          : {}),
       })
       .catch(async (err) => {
         if (err instanceof LumaAI.APIError) {
